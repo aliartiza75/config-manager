593de8c2a217736e33c4c71a936af24795543264
Irtiza Ali <aliartiza75@yahoo.com>
2018-11-08 00:07:40 +0500

    [folder-structure] I have added the folder strucutre

---
+++ app/services/kudu_service.py
@@ -0,0 +1,12 @@
+###################################################################################################
+# Summary: File contains code for configuration manager service
+###################################################################################################
+
+
+class ConfigurationManager:
+
+    def __init__(self):
+        '''
+        Configurations Manager class constructor
+        '''
+        config = 1
---
+++ app/services/response_service.py
@@ -0,0 +1,15 @@
+###################################################################################################
+# Summary: This file contains response service
+###################################################################################################
+
+status = {"OK": 200,
+          "CREATED": 201,
+          "BAD_REQUEST": 400,
+          "DELETED": 204,
+          "UNAUTHORIZED": 401,
+          "NOT_MODIFIED": 403,
+          "NOT_FOUND": 404,
+          "UNPROCESSABLE": 422,
+          "INTERNAL_SERVER_ERROR": 500,
+          "ALTERED": 201
+          }

1d6058c6095e8e46d46268c3d8c583ba69eeb2ef
Irtiza Ali <aliartiza75@yahoo.com>
2018-11-12 00:53:39 +0500

    [configure-db] added .gitignore file and also updated the app/__init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.kudu_endpoints.routes import mod
+from app.config_manager_endpoints.routes import mod
 from flask import Flask, Blueprint
 from flask_api import FlaskAPI
 app = Flask(__name__)
